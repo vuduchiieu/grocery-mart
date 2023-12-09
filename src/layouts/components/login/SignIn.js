79da812e290c31377a10a0047f111d6140fbdefe
vuduchiieu <vuduchiieu@gmail.com>
2023-12-10 00:49:52 +0700

    fix màu đổi tên

--- src/layouts/components/login/SignIn.js
+++ src/layouts/components/login/SignIn.js
@@ -22,7 +22,7 @@ function SignIn() {
                 <div className={cx("wrap")}>
                     <Link to={"/"} className={cx("logo")}>
                         <img src={icon.logo} />
-                        <h1>siêuTạpHóa</h1>
+                        <h1>siêuTapHóa</h1>
                     </Link>
                     <div className={cx("title")}>
                         <h3>Hello Again!</h3>
--- src/layouts/components/login/SignUp.js
+++ src/layouts/components/login/SignUp.js
@@ -22,7 +22,7 @@ function SignUp() {
                 <div className={cx("wrap")}>
                     <Link to={"/"} className={cx("logo")}>
                         <img src={icon.logo} />
-                        <h1>siêuTạpHóa</h1>
+                        <h1>siêuTapHóa</h1>
                     </Link>
                     <div className={cx("title")}>
                         <h3>Sign Up</h3>
